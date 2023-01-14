98425dd587bc41576b758c519d380b4bdd30d945
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-01-14 13:05:12 -0500

    Routes and started redux reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
 import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import NavBar from "./scenes/global/NavBar";
 //Always start at top, even if you click on new link
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -13,8 +17,12 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <ScrollToTop />
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={ItemDetails} />
+          <Route path="checkout" element={Checkout} />
+          <Route path="checkout/success" element={Confirmation} />
         </Routes>
       </BrowserRouter>
     </div>
